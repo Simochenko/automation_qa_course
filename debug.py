47273a814e04eda13aaedc61e575d69f0fc128c2
svsimon <svsimon@mail.ru>
2023-11-20 22:34:01 +0200

    update elements tests added tests: -test_web_table_update_person_info -test_web_table_delete_person -test_web_table_change_count_row added debug.py

---
+++ debug.py
@@ -0,0 +1,10 @@
+# data1 = ['angular', 'veu', 'office', 'public', 'private', 'classified', 'general']
+# data2 = ['Angular', 'Veu', 'Office', 'Public', 'Private', 'Classified', 'General']
+#
+# print(str(data1).replace(' ', '').replace('doc', '').replace('.', '').lower())
+# print(data2)
+#
+# data1 = str(data1).replace(' ', '').replace('doc', '').replace('.', '').lower()
+# data2 = str(data2).replace(' ', '').lower()
+#
+# assert data1 == data2
