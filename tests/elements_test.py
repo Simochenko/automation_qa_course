978fe7ae6e71ff53afd8fdab3ac997456992dba6
svsimon <svsimon@mail.ru>
2023-11-21 22:28:09 +0200

    update elements tests added class: -TestButtonsPage added tests: -test_different_click_on_the_buttons update BasePage: added def: -action_double_click -action_rigth_click

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage
 
 
 # from pages.base_page import BasePage
@@ -69,6 +69,7 @@ class TestElements:
             table_result = web_table_page.check_search_person()
             assert key_word in table_result, "the person was not found in the table"
 
+        # ---------------------------------------------------------------------------------------------------------------------
         def test_web_table_update_person_info(self, driver):
             web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
             web_table_page.open()
@@ -92,4 +93,16 @@ class TestElements:
             web_table_page.open()
             count = web_table_page.select_up_to_some_rows()
             assert count == [5, 10, 20, 25, 50,
-                             100], 'The number of rows in the table has not been changed or has charged incorrectly '
+                             100], 'The number of rows in the table has not been changed or has charged incorrectly'
+
+    class TestButtonsPage:
+        def test_different_click_on_the_buttons(self, driver):
+            button_page = ButtonsPage(driver, 'https://demoqa.com/buttons')
+            button_page.open()
+            double = button_page.click_on_different_button('double')
+            right = button_page.click_on_different_button('right')
+            click = button_page.click_on_different_button('click')
+            assert double == "You have done a double click", "The double click button was not pressed"
+            assert right == "You have done a right click", "The right click button was not pressed"
+            assert click == "You have done a dynamic click", "The dynamic click button was not pressed"
+
