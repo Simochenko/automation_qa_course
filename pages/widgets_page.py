3c2cddefce5a334184a8837ea04989d5cc74798b
svsimon <svsimon@mail.ru>
2023-12-01 00:09:59 +0200

    added tests: -test_tabs

--- pages/widgets_page.py
+++ pages/widgets_page.py
@@ -153,31 +153,31 @@ class ProgressBarPage(BasePage):
         return value_before, value_after
 
 
-# class TabsPage(BasePage):
-#     locators = TabsPageLocators()
-#
-#     # @allure.step('check tabs')
-#     def check_tabs(self, name_tab):
-#         tabs = {'what':
-#                     {'title': self.locators.TABS_WHAT,
-#                      'content': self.locators.TABS_WHAT_CONTENT},
-#                 'origin':
-#                     {'title': self.locators.TABS_ORIGIN,
-#                      'content': self.locators.TABS_ORIGIN_CONTENT},
-#                 'use':
-#                     {'title': self.locators.TABS_USE,
-#                      'content': self.locators.TABS_USE_CONTENT},
-#                 'more':
-#                     {'title': self.locators.TABS_MORE,
-#                      'content': self.locators.TABS_MORE_CONTENT},
-#                 }
-#
-#         button = self.element_is_visible(tabs[name_tab]['title'])
-#         button.click()
-#         what_content = self.element_is_visible(tabs[name_tab]['content']).text
-#         return button.text, len(what_content)
-#
-#
+class TabsPage(BasePage):
+    locators = TabsPageLocators()
+
+    # @allure.step('check tabs')
+    def check_tabs(self, name_tab):
+        tabs = {'what':
+                    {'title': self.locators.TABS_WHAT,
+                     'content': self.locators.TABS_WHAT_CONTENT},
+                'origin':
+                    {'title': self.locators.TABS_ORIGIN,
+                     'content': self.locators.TABS_ORIGIN_CONTENT},
+                'use':
+                    {'title': self.locators.TABS_USE,
+                     'content': self.locators.TABS_USE_CONTENT},
+                'more':
+                    {'title': self.locators.TABS_MORE,
+                     'content': self.locators.TABS_MORE_CONTENT},
+                }
+
+        button = self.element_is_visible(tabs[name_tab]['title'])
+        button.click()
+        what_content = self.element_is_visible(tabs[name_tab]['content']).text
+        return button.text, len(what_content)
+
+
 # class ToolTipsPage(BasePage):
 #     locators = ToolTipsPageLocators()
 #
