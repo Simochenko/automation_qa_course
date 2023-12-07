47f0f1fb5460a6f80c7a8edb7f4036b189009512
svsimon <svsimon@mail.ru>
2023-12-07 22:48:37 +0200

    added steps in allure added screenshot update conftest.py

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -25,11 +25,13 @@ class TextBoxPage(BasePage):
         email = person_info.email
         current_address = person_info.current_address
         permanent_address = person_info.permanent_address
-        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
-        self.element_is_visible(self.locators.EMAIL).send_keys(email)
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
-        self.element_is_visible(self.locators.SUBMIT).click()
+        with allure.step('filing fields'):
+            self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+            self.element_is_visible(self.locators.EMAIL).send_keys(email)
+            self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+            self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
+        with allure.step('click submit button'):
+            self.element_is_visible(self.locators.SUBMIT).click()
         return full_name, email, current_address, permanent_address
 
     @allure.step('check filled form')
